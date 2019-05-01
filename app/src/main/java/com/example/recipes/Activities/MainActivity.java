4ce33d2dc3f7bf8a6ea8900d7a58bd86d46a3c10
Leonardo Pinheiro <leonardo.pinheiro@ruptiva.com>
2019-05-01 19:42:47 -0300

    update (project) structure

--- app/src/main/java/com/example/recipes/Activities/MainActivity.java
+++ app/src/main/java/com/example/recipes/Activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.recipes;
+package com.example.recipes.Activities;
 
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import com.example.recipes.R;
 
 public class MainActivity extends AppCompatActivity {
     private TextView mTextMessage;
