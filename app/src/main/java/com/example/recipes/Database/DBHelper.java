d0d79bc6dbbd6af5f06ca909ad8ddc47a27b9a59
Leonardo Pinheiro <leonardo.pinheiro@ruptiva.com>
2019-05-08 14:35:27 -0300

    add (Recipe) CRUD

--- app/src/main/java/com/example/recipes/Database/DBHelper.java
+++ app/src/main/java/com/example/recipes/Database/DBHelper.java
@@ -5,6 +5,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import com.example.recipes.DAO.CategoryDAO;
+import com.example.recipes.DAO.RecipeDAO;
 
 public class DBHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "recipes_db";
@@ -16,6 +17,7 @@ public class DBHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("create table " + CategoryDAO.TABLE + " ( " + CategoryDAO.ID + " integer primary key autoincrement, "  + CategoryDAO.NAME + " varchar(100) not null, " + CategoryDAO.DESCRIPTION + " varchar(100));");
+        db.execSQL("create table " + RecipeDAO.TABLE + " ( " + RecipeDAO.ID + " integer primary key autoincrement, " + RecipeDAO.CATEGORY_ID + " integer not null, " + RecipeDAO.NAME + " varchar(100) not null, " + RecipeDAO.TIME + " integer not null, " + RecipeDAO.YIELD + " integer not null, " + RecipeDAO.INGREDIENTS + " varchar(1000) not null, " + RecipeDAO.METHOD_OF_PREPARATION + " varchar(1000) not null);");
     }
 
     @Override
